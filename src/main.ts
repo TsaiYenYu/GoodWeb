6446d09b260de96587cd6dc94e2138a3a79673b4
CLMario <178ssn@gmail.com>
2022-10-20 13:05:46 +0800

    setup NativeUI in main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,26 @@ import { createPinia } from "pinia";
 import '@fortawesome/fontawesome-free/js/all'
 import App from "./App.vue";
 import router from "./router";
+import 'vfonts/Lato.css'
+// 等宽字体
+import 'vfonts/FiraCode.css'
+import {
+    // create naive ui
+    create,
+    // component
+    NButton
+  } from 'naive-ui'
 
 import "./assets/main.css";
+const naive = create({
+    components: [NButton]  
+})
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(naive)
 
 app.mount("#app");
+
